75bb2ced76451b31e99d74570617d4f24700c0a0
Erhan <erhantezer68@gmail.com>
2022-07-15 00:43:25 +0300

    new project

---
+++ src/pages/home/Home.style.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ImgDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 1.5rem;
+`;
+
+export const HomeImg = styled.img`
+  width: 80%;
+  max-width: 750px;
+`;
+
+export const HeaderText = styled.h1`
+  font-size: 2.5rem;
+  color: red;
+  text-align: center;
+`;
+const APP_ID ="f6edb9f6";
+const APP_KEY ="4ab957a75da653a00a88eb87864b8139";
